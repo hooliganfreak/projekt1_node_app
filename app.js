9c1054f885c9bafa22ad117fd070977e73d715aa
hooliganfreak <97944477+hooliganfreak@users.noreply.github.com>
2024-10-14 12:51:37 +0300

    Update app.js PORT

--- app.js
+++ app.js
@@ -533,6 +533,8 @@ app.delete('/boards/:id', authenticateJWT, async (req, res) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
